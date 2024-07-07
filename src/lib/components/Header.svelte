c2b60615c303a41dca68556f62ec9e13c826f34d
principle105 <principle105@gmail.com>
2024-07-07 17:42:07 -0400

    boiler plate

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import logo from "$lib/images/logo.jpg";
+</script>
+
+<header class="flex justify-between items-center py-9 sm:py-10 z-50">
+    <div class="flex gap-12 items-center">
+        <a class="flex gap-4 sm:gap-6 items-center" href="/">
+            <img src={logo} alt="Logo" class="size-12" />
+            <h3 class="text-xl sm:text-2xl font-bold tracking-wide">
+                Life Coin
+            </h3>
+        </a>
+
+        <nav class="gap-12 items-center text-zinc-600 hidden lg:flex">
+            <a href="/">Dashboard</a>
+            <a href="/leaderboards">About</a>
+        </nav>
+    </div>
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import "./global.css";
+
+    import Header from "$lib/components/Header.svelte";
+</script>
+
+<div
+    class="grid min-h-screen px-12 xl:px-24"
+    style="grid-template-rows: auto 1fr auto;"
+>
+    <Header />
+
+    <main class="w-full h-full">
+        <slot />
+    </main>
+</div>
